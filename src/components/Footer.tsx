061ebd7332a2a4e6a9784e3e2a37a1d7ec18be92
me <blindtone@hotmail.com>
2024-07-29 09:18:06 -0400

    Updated footer to remove Blog; clients=>experience

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -30,9 +30,8 @@ export function Footer() {
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/about">About</NavLink>
-                <NavLink href="/clients">Clients</NavLink>
+                <NavLink href="/clients">Experience</NavLink>
                 <NavLink href="/services">Services</NavLink>
-                <NavLink href="/blog">Blog</NavLink>
                 <NavLink href="/publicity">Publicity</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
